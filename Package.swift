f60d574f5ce65363389d8668b8d1721f29d5e46a
Benjamin Komen <benjamin.komen@generac.com>
2026-05-29 19:19:05 -0700

    feat(ios): add Package.swift for direct Swift Package Manager consumption

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+  name: "ReactNativeLocalNetworkPermission",
+  platforms: [
+    .iOS(.v16)
+  ],
+  products: [
+    .library(
+      name: "ReactNativeLocalNetworkPermission",
+      targets: ["ReactNativeLocalNetworkPermission"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/expo/expo.git", from: "56.0.0")
+  ],
+  targets: [
+    .target(
+      name: "ReactNativeLocalNetworkPermission",
+      dependencies: [
+        .product(name: "ExpoModulesCore", package: "expo")
+      ],
+      path: "ios",
+      exclude: [
+        "ReactNativeLocalNetworkPermission.podspec"
+      ],
+      sources: [
+        "ReactNativeLocalNetworkPermissionModule.swift",
+        "LocalNetworkAuthorization.swift"
+      ],
+      linkerSettings: [
+        .linkedFramework("Network")
+      ]
+    )
+  ]
+)
